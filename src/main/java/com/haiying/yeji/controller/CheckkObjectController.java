91dddbb2d23ba1f70c5ed47812f28797ae24de4d
jonny201251 <jonny201251@163.com>
2021-12-10 19:47:34 +0800

    a

--- src/main/java/com/haiying/yeji/controller/CheckkObjectController.java
+++ src/main/java/com/haiying/yeji/controller/CheckkObjectController.java
@@ -1,10 +1,20 @@
 package com.haiying.yeji.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.haiying.yeji.common.result.ResponseResultWrapper;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.haiying.yeji.model.entity.CheckkObject;
+import com.haiying.yeji.model.vo.CheckkObjectVO;
+import com.haiying.yeji.service.CheckkObjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * <p>
@@ -18,5 +28,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/checkkObject")
 @ResponseResultWrapper
 public class CheckkObjectController {
+    @Autowired
+    CheckkObjectService checkkObjectService;
 
+    @GetMapping("list")
+    public IPage<CheckkObject> list(int current, int pageSize) {
+        QueryWrapper<CheckkObject> wrapper = new QueryWrapper<CheckkObject>().select("distinct checkk_object");
+        return checkkObjectService.page(new Page<>(current, pageSize), wrapper);
+    }
+
+    @PostMapping("add")
+    public boolean add(@RequestBody CheckkObjectVO checkkObjectVO) {
+        for (CheckkObject checkkObject : checkkObjectVO.getCheckList()) {
+            //edit
+            checkkObject.setId(null);
+
+            checkkObject.setCheckkObject(checkkObjectVO.getCheckkObject());
+        }
+        return checkkObjectService.saveBatch(checkkObjectVO.getCheckList());
+    }
+
+    @GetMapping("get")
+    public CheckkObjectVO get(String checkkObject) {
+        CheckkObjectVO checkkObjectVO = new CheckkObjectVO();
+        checkkObjectVO.setCheckkObject(checkkObject);
+        List<CheckkObject> list = checkkObjectService.list(new LambdaQueryWrapper<CheckkObject>().eq(CheckkObject::getCheckkObject, checkkObject));
+        checkkObjectVO.setCheckList(list);
+        return checkkObjectVO;
+    }
+
+    @PostMapping("edit")
+    public boolean edit(@RequestBody CheckkObjectVO checkkObjectVO) {
+        //先删除
+        checkkObjectService.remove(new LambdaQueryWrapper<CheckkObject>().eq(CheckkObject::getCheckkObject, checkkObjectVO.getCheckkObject()));
+        //后插入
+        add(checkkObjectVO);
+        return true;
+    }
+
+    @GetMapping("delete")
+    public boolean delete(String[] arr) {
+        List<String> list = Stream.of(arr).collect(Collectors.toList());
+        return checkkObjectService.remove(new LambdaQueryWrapper<CheckkObject>().in(CheckkObject::getCheckkObject, list));
+    }
 }
--- src/main/java/com/haiying/yeji/controller/CheckkProjectController.java
+++ src/main/java/com/haiying/yeji/controller/CheckkProjectController.java
@@ -1,9 +1,20 @@
 package com.haiying.yeji.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.haiying.yeji.common.result.ResponseResultWrapper;
+import com.haiying.yeji.model.entity.CheckkProject;
+import com.haiying.yeji.model.vo.CheckkProjectVO;
+import com.haiying.yeji.service.CheckkProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * <p>
@@ -14,7 +25,47 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-09
  */
 @RestController
-@RequestMapping("/checkk-project")
+@RequestMapping("/checkkProject")
+@ResponseResultWrapper
 public class CheckkProjectController {
+    @Autowired
+    CheckkProjectService checkkProjectService;
 
+    @GetMapping("list")
+    public IPage<CheckkProject> list(int current, int pageSize) {
+        QueryWrapper<CheckkProject> wrapper = new QueryWrapper<CheckkProject>().select("distinct checkk_type");
+        return checkkProjectService.page(new Page<>(current, pageSize), wrapper);
+    }
+
+    @PostMapping("add")
+    public boolean add(@RequestBody CheckkProjectVO checkkProjectVO) {
+        for (CheckkProject checkkProject : checkkProjectVO.getCheckList()) {
+            checkkProject.setCheckkType(checkkProjectVO.getCheckkType());
+        }
+        return checkkProjectService.saveBatch(checkkProjectVO.getCheckList());
+    }
+
+    @GetMapping("get")
+    public CheckkProjectVO get(String checkkType) {
+        CheckkProjectVO checkkProjectVO = new CheckkProjectVO();
+        checkkProjectVO.setCheckkType(checkkType);
+        List<CheckkProject> list = checkkProjectService.list(new LambdaQueryWrapper<CheckkProject>().eq(CheckkProject::getCheckkType, checkkType));
+        checkkProjectVO.setCheckList(list);
+        return checkkProjectVO;
+    }
+
+    @PostMapping("edit")
+    public boolean edit(@RequestBody CheckkProjectVO checkkProjectVO) {
+        //先删除
+        checkkProjectService.remove(new LambdaQueryWrapper<CheckkProject>().eq(CheckkProject::getCheckkType, checkkProjectVO.getCheckkType()));
+        //后插入
+        add(checkkProjectVO);
+        return true;
+    }
+
+    @GetMapping("delete")
+    public boolean delete(String[] arr) {
+        List<String> list = Stream.of(arr).collect(Collectors.toList());
+        return checkkProjectService.remove(new LambdaQueryWrapper<CheckkProject>().in(CheckkProject::getCheckkType, list));
+    }
 }
