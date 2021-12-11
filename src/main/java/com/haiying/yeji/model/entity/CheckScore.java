ea0daff0d987ea84e1496a15263849081afefd6c
jonny201251 <jonny201251@163.com>
2021-12-11 21:41:18 +0800

    a

--- src/main/java/com/haiying/yeji/model/entity/CheckScore.java
+++ src/main/java/com/haiying/yeji/model/entity/CheckScore.java
@@ -3,9 +3,10 @@ package com.haiying.yeji.model.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 /**
  * <p>
